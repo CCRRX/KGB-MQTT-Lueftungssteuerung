4dd4fda89b5d6471e130ca736d7e0f3248c16e57
CCRRX <jan.koppatscheck@gmail.com>
2020-03-25 19:14:37 +0100

    Update LuefungMQTTRelais.ino

--- LuefungMQTTRelais.ino
+++ LuefungMQTTRelais.ino
@@ -1,61 +1,100 @@
-/*
- Publishing in the callback
-  - connects to an MQTT server
-  - subscribes to the topic "inTopic"
-  - when a message is received, republishes it to "outTopic"
-  This example shows how to control pins using Ethernet shield via MQTT
-*/
-
-//#include <SPI.h>
-//#include <Ethernet.h>
-
-
 #include <UIPEthernet.h>
-// The connection_data struct needs to be defined in an external file.
 #include <UIPServer.h>
 #include <UIPClient.h>
-
-
 #include <PubSubClient.h>
 
-// Update these with values suitable for your network.
-byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED }; // Best MAC ^^
 IPAddress ip(192, 168, 178, 177);
 IPAddress server(192, 168, 178, 128);
 
-const int lightPin = 2;
+const int RelayPin0 = 2;
+const int RelayPin1 = 3;
+const int RelayPin2 = 4;
+const int RelayPin3 = 5;
+const int RelayPin4 = 6;
+const int RelayPin5 = 7;
+const int RelayPin6 = 8;
+const int RelayPin7 = 9;
 
-// Callback function header
 void callback(char* topic, byte* payload, unsigned int length);
 
 EthernetClient ethClient;
 PubSubClient client(server, 1883, callback, ethClient);
 
-// Callback function
+
 void callback(char* topic, byte* payload, unsigned int length) {
-    
-  //turn the LED ON if the payload is '1' and publish to the MQTT server a confirmation message
-  if(payload[0] == '1'){
-    digitalWrite(lightPin, HIGH);
-    client.publish("testTopic", "Light On"); }
-    
-  //turn the LED OFF if the payload is '0' and publish to the MQTT server a confirmation message
-  if (payload[0] == '0'){
-    digitalWrite(lightPin, LOW);
-    client.publish("testTopic", "Light Off");
+
+    char msg[length+1];
+    for (int i = 0; i < length; i++) {
+        msg[i] = (char)payload[i];
     }
-} // void callback
+    msg[length] = '\0';
+    
+    if(strcmp(msg,"00")==0){
+      digitalWrite(RelayPin0, HIGH);}
+    
+    if (strcmp(msg,"01")==0){
+      digitalWrite(RelayPin0, LOW);}
+
+    if(strcmp(msg,"10")==0){
+      digitalWrite(RelayPin1, HIGH);}
+    
+    if (strcmp(msg,"11")==0){
+      digitalWrite(RelayPin1, LOW);}
+
+    if(strcmp(msg,"20")==0){
+      digitalWrite(RelayPin2, HIGH);}
+    
+    if (strcmp(msg,"21")==0){
+      digitalWrite(RelayPin2, LOW);}
+
+    if(strcmp(msg,"30")==0){
+      digitalWrite(RelayPin3, HIGH);}
+    
+    if (strcmp(msg,"31")==0){
+      digitalWrite(RelayPin3, LOW);}
+ 
+    if(strcmp(msg,"40")==0){
+      digitalWrite(RelayPin4, HIGH);}
+    
+    if (strcmp(msg,"41")==0){
+      digitalWrite(RelayPin4, LOW);}
+
+    if(strcmp(msg,"50")==0){
+      digitalWrite(RelayPin5, HIGH);}
+    
+    if (strcmp(msg,"51")==0){
+      digitalWrite(RelayPin5, LOW);}
+
+    if(strcmp(msg,"60")==0){
+      digitalWrite(RelayPin6, HIGH);}
+    
+    if (strcmp(msg,"61")==0){
+      digitalWrite(RelayPin6, LOW);}
+
+    if(strcmp(msg,"70")==0){
+      digitalWrite(RelayPin7, HIGH);}
+    
+    if (strcmp(msg,"71")==0){
+      digitalWrite(RelayPin7, LOW);}
+}
 
 
 void setup()
 {
-  pinMode(lightPin, OUTPUT);
-  digitalWrite(lightPin, LOW);
+
+int outMin = 2; // Lowest input pin
+int outMax = 9; // Highest input pin
+for(int i=outMin; i<=outMax; i++)
+{
+  pinMode(i, OUTPUT);
+  digitalWrite(i, HIGH);
+}
   
   Ethernet.begin(mac, ip);
-  if (client.connect("arduinoClient")) {
-    client.publish("testTopic","hello world");
-    client.subscribe("inTopic");
+  if (client.connect("FU-Client")) {
+    client.publish("/lueftung/fu","FU-Client Booted");
+    client.subscribe("/lueftung/fu");
   }
 }
 
